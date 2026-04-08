fd0e9d620f166af35b2f0922eed266b405f6fe49
Deo Cherisme <deolenscherisme1000@gmail.com>
2026-04-08 05:39:43 -0400

    connect express server to postgresql database

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import pool from './db';
 
 dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+pool.query('SELECT NOW()', (err, res) =>{
+    if (err) {
+        console.log('Database connection failed', err);
+    } else {
+        console.log('Database connected: ', res.rows[0]);
+    }
+});
 
 app.get('/', (req, res) => {
     res.json({ message: 'Hello Word!' });
